b06bf5c1d4bc1013c95b2b1de8c901a44d58b2f3
Leonardo Alves <77860170+leomonadas@users.noreply.github.com>
2023-07-26 21:33:30 -0300

    Desafios opcionais

---
+++ Curso Compile/desafiosOpcionais/src/desafiosOpcionais/multiplosDeTres.java
@@ -0,0 +1,13 @@
+package desafiosOpcionais;
+
+public class multiplosDeTres {
+	public static void main(String[] args) {
+		int numero = 0;
+		while(numero <= 100) {
+			if(numero % 3 == 0) {
+				  System.out.println(numero);
+			}
+			numero++;
+		}
+	}
+}
